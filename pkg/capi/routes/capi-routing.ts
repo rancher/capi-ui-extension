51566eb96b58e1623e3a2a59ff85651bd6ab167e
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2023-12-13 13:00:32 -0800

    remove test page

--- pkg/capi/routes/capi-routing.ts
+++ pkg/capi/routes/capi-routing.ts
@@ -1,16 +1,10 @@
 import Dashboard from '../pages/index.vue';
-import blerg from '../pages/blerg.vue';
 
 const routes = [
   {
     name:      'c-cluster-manager-capi',
     path:      '/c/:cluster/manager/capi',
     component: Dashboard,
-  },
-  {
-    name:      'c-cluster-manager-capi-blerg',
-    path:      '/c/:cluster/manager/capi/blerg',
-    component: blerg,
   },
 ];
 
