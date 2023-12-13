8b2921214ea0419b12a32db16096981dc94fd93a
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2023-12-13 11:55:26 -0800

    more validators

--- pkg/capi/components/CCVariables/data.ts
+++ pkg/capi/components/CCVariables/data.ts
@@ -1,19 +1,19 @@
 export default [{
-  name:     'region',
-  required: false,
+  name:     'string-required',
+  required: true,
   schema:
     {
       openAPIV3Schema:
       {
         type:        'string',
-        // default:     'us-east-1',
-        description: 'The Azure region to create machines in'
+        default:     'us-east-1',
+        description: 'This string has only required validation'
       }
     }
 },
 
 {
-  name:     'sshKeyName',
+  name:     'string-min-length',
   required: true,
   schema:
     {
@@ -21,32 +21,101 @@ export default [{
       {
         type:        'string',
         default:     'default',
-        description: 'SSH key name description text go here',
-        example:     'sshkeyexample'
+        description: 'this string has a rule that its length must be greater than or equal to 3',
+        example:     'sshkeyexample',
+        minLength:   3
       }
     }
 },
 {
-  name:     'controlPlaneMachineType',
+  name:     'string-max-length',
   required: true,
   schema:
     {
       openAPIV3Schema:
       {
-        type:    'string',
-        default: 't3.large'
+        type:        'string',
+        default:     't3.large',
+        description: 'this string has a rule that its length must be less than or equal to 10',
+        maxLength:   10
       }
     }
 },
 {
-  name:     'workerMachineType',
+  name:     'string-min-max-length',
   required: true,
   schema:
     {
       openAPIV3Schema:
       {
-        type:    'string',
-        default: 't3.large'
+        type:        'string',
+        default:     't3.large',
+        description: 'this string has a rules that its length must be between 3 and 10',
+        maxLength:   10,
+        minLength:   3
+      }
+    }
+},
+
+{
+  name:     'string-pattern',
+  required: true,
+  schema:
+    {
+      openAPIV3Schema:
+      {
+        type:        'string',
+        default:     'default',
+        description: 'this string must match the pattern /^[a-z]+-[0-9]+$/',
+        example:     'sshkeyexample',
+        pattern:     '^[a-z]+-[0-9]+$'
+      }
+    }
+},
+{
+  name:     'string-no-validate',
+  required: false,
+  schema:
+    {
+      openAPIV3Schema:
+      {
+        type:        'string',
+        default:     't3.large',
+        description: 'this string has no validation'
+      }
+    }
+},
+
+{
+  name:     'number-exclusive-min-max',
+  required: false,
+  schema:
+    {
+      openAPIV3Schema:
+      {
+        type:             'number',
+        default:          3,
+        description:      'this string has exclusive min max of 2 and 5',
+        exclusiveMinimum: true,
+        exclusiveMaximum: true,
+        minimum:          2,
+        maximum:          5
+      }
+    }
+},
+
+{
+  name:     'number-inclusive-min-max',
+  required: false,
+  schema:
+    {
+      openAPIV3Schema:
+      {
+        type:        'number',
+        default:     3,
+        description: 'this string has inclusive min max of 2 and 5',
+        minimum:     2,
+        maximum:     5
       }
     }
 },
@@ -63,15 +132,30 @@ export default [{
     }
 },
 {
-  name:     'testArrayStrings',
+  name:     'testArrayStrings-min',
   schema:
     {
       openAPIV3Schema:
       {
         type:        'array',
-        description: 'This is a test array value',
+        description: 'This array must have at least 3 items',
         items:
-          { type: 'string' }
+          { type: 'string' },
+        minItems: 3
+      }
+    }
+},
+{
+  name:     'testArrayStrings-max',
+  schema:
+    {
+      openAPIV3Schema:
+      {
+        type:        'array',
+        description: 'This array must have at most 5 items',
+        items:
+          { type: 'string' },
+        maxItems: 5
       }
     }
 },
@@ -95,9 +179,10 @@ export default [{
       openAPIV3Schema:
       {
         type:        'array',
-        description: 'This is an array type with enum defined',
+        description: 'This is an array type with enum defined. It may have at most two elements',
         enum:
-          ['option_1', 'option_2', 'option_3']
+          ['option_1', 'option_2', 'option_3'],
+        maxItems: 2
       }
     }
 },
@@ -113,4 +198,5 @@ export default [{
           [{ option_1: '1' }, { option_2: '2' }, { option_3: '3' }]
       }
     }
-}];
+},
+];
---
+++ pkg/capi/util/validators.ts
@@ -0,0 +1,105 @@
+// * format (string)
+// * exclusiveMinimum (integer/number)
+// * exclusiveMaximum (integer/number)
+// * maxItems (array)
+// * maxLength (string)
+// * maximum (integer/number)
+// * minItems (array)
+// * minLength (string)
+// * minimum (integer/number)
+// * pattern (string)
+// * uniqueItems (array)
+// * required (object)
+
+import formRulesGenerator, { Validator, ValidationOptions, ValidatorFactory } from '@shell/utils/validators/formRules';
+import { Translation } from '@shell/types/t';
+
+// const formatPatternMap = {
+//   "bsonobjectid", // bson object ID
+// 	"uri",          // an URI as parsed by Golang net/url.ParseRequestURI
+// 	"email",        // an email address as parsed by Golang net/mail.ParseAddress
+// 	"hostname",     // a valid representation for an Internet host name, as defined by RFC 1034, section 3.1 [RFC1034].
+// 	"ipv4",         // an IPv4 IP as parsed by Golang net.ParseIP
+// 	"ipv6",         // an IPv6 IP as parsed by Golang net.ParseIP
+// 	"cidr",         // a CIDR as parsed by Golang net.ParseCIDR
+// 	"mac",          // a MAC address as parsed by Golang net.ParseMAC
+// 	"uuid":         /(?i)^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}$/,
+// 	"uuid3",        // an UUID3 that allows uppercase defined by the regex (?i)^[0-9a-f]{8}-?[0-9a-f]{4}-?3[0-9a-f]{3}-?[0-9a-f]{4}-?[0-9a-f]{12}$
+// 	"uuid4",        // an UUID4 that allows uppercase defined by the regex (?i)^[0-9a-f]{8}-?[0-9a-f]{4}-?4[0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$
+// 	"uuid5",        // an UUID6 that allows uppercase defined by the regex (?i)^[0-9a-f]{8}-?[0-9a-f]{4}-?5[0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$
+// 	"isbn",         // an ISBN10 or ISBN13 number string like "0321751043" or "978-0321751041"
+// 	"isbn10",       // an ISBN10 number string like "0321751043"
+// 	"isbn13",       // an ISBN13 number string like "978-0321751041"
+// 	"creditcard":   /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$/,
+// 	"ssn",          // a U.S. social security number following the regex ^\\d{3}[- ]?\\d{2}[- ]?\\d{4}$
+// 	"hexcolor",     // an hexadecimal color code like "#FFFFFF", following the regex ^#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$
+// 	"rgbcolor",     // an RGB color code like rgb like "rgb(255,255,2559"
+// 	"byte",         // base64 encoded binary data
+// 	"password",     // any kind of string
+// 	"date",         // a date string like "2006-01-02" as defined by full-date in RFC3339
+// 	"duration",     // a duration string like "22 ns" as parsed by Golang time.ParseDuration or compatible with Scala duration format
+// 	"datetime",     // a date time string like "2014-12-15T19:30:20.000Z" as defined by date-time in RFC3339
+// }
+
+export default function(t: Translation, { key = 'Value' }: ValidationOptions, openAPIV3Schema: any): { [key:string]: Validator<any> | ValidatorFactory } {
+  const {
+    format,
+    exclusiveMinimum,
+    exclusiveMaximum,
+    maxItems,
+    maxLength,
+    maximum,
+    minItems,
+    minLength,
+    minimum,
+    pattern,
+    uniqueItems,
+    required: requiredFields
+  } = openAPIV3Schema;
+
+  const builtInRules = formRulesGenerator(t, { key } );
+  const out = [] as any;
+
+  if (maximum) {
+    if (exclusiveMaximum) {
+      out.push((val: string | number) => Number(val) >= Number(maximum) ? t('validation.exclusiveMaxValue', { key, maximum }) : undefined);
+    } else {
+      // out.push(builtInRules.maxValue(maximum));
+    }
+  }
+
+  if (minimum !== undefined) {
+    if (exclusiveMinimum) {
+      out.push((val: string | number) => Number(val) <= Number(minimum) ? t('validation.exclusiveMinValue', { key, minimum }) : undefined);
+    } else {
+      // out.push(builtInRules.minValue(minimum));
+    }
+  }
+  if (minLength !== undefined) {
+    // out.push(builtInRules.minLength(minLength));
+  }
+  if (maxLength) {
+    // out.push(builtInRules.maxLength(maxLength));
+  }
+  if (maxItems) {
+    out.push((val: any[]) => val?.length > maxItems ? t('validation.maxItems', { key, maxItems }) : undefined);
+  }
+
+  if (minItems !== undefined) {
+    out.push((val: any[]) => val?.length < minItems ? t('validation.minItems', { key, minItems }) : undefined);
+  }
+
+  if (pattern) {
+    out.push((val: string) => val && !val.match(new RegExp(pattern)) ? t('validation.pattern', { key, pattern }) : undefined);
+  }
+
+  if (uniqueItems) {
+    out.push((val: any[]) => val.filter((item, index) => val.indexOf(item) !== index)?.length ? t('validation.uniqueItems', { key }) : undefined);
+  }
+
+  if (requiredFields) {
+    out.push((val: any) => requiredFields.find((key: string) => val[key] === undefined) ? t('validation.requiredFields', { key, requiredFields: requiredFields.toString() }) : undefined);
+  }
+
+  return out;
+}
