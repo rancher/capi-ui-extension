62fe9f40373b578c1d4285006d483536014194f4
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2023-12-20 09:32:29 -0800

    re-set variables when clusterclass changes

---
+++ pkg/capi/edit/cluster.x-k8s.io.cluster.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Tabbed from '@shell/components/Tabbed';
+import Tab from '@shell/components/Tabbed/Tab';
+import LabeledSelect from '@shell/components/form/LabeledSelect';
+
+import { CAPI } from '../types/capi';
+import CCVariables from '../components/CCVariables';
+const TEST_CC_ID = 'default/quickstart-more-variables';
+
+export default defineComponent({
+  name:       'EditCapiCluster',
+  components: {
+    CCVariables, Tabbed, Tab, LabeledSelect
+  },
+  props: {
+    value: {
+      type:    Object,
+      default: () => {
+        return {};
+      }
+    }
+  },
+  async fetch() {
+    this.$set(this.value, 'spec', { topology: { variables: [] } });
+    this.clusterClasses = await this.$store.dispatch('management/findAll', { type: CAPI.CLUSTER_CLASS });
+  },
+  data() {
+    return {
+      clusterClassId: null, clusterClasses: [], variables: [], variablesValid: true
+    };
+  },
+
+  computed: {
+
+    clusterClassOptions() {
+      return (this.clusterClasses || []).map((cc: any) => {
+        return { label: cc.metadata.name, value: cc.id };
+      });
+    },
+    clusterClass() {
+      return this.clusterClassId && this.clusterClasses ? this.clusterClasses.find(cc => cc.id === this.clusterClassId) : null;
+    }
+  }
+});
+</script>
+
+<template>
+  <div>
+    <div>Is valid: {{ variablesValid }}</div>
+    <LabeledSelect v-if="clusterClasses" v-model="clusterClassId" :options="clusterClassOptions" />
+    <Tabbed :side-tabs="true">
+      <Tab name="variables">
+        <CCVariables v-if="clusterClass" v-model="variables" :cluster-class="clusterClass" @validation-passed="e=>variablesValid=e" />
+      </tab>
+    </Tabbed>
+  </div>
+</template>
--- pkg/capi/util/validators.ts
+++ pkg/capi/util/validators.ts
@@ -1,5 +1,54 @@
 import { Validator, ValidationOptions } from '@shell/utils/validators/formRules';
 import { Translation } from '@shell/types/t';
+
+// const stringFormats = {
+//   // this is a mongodb id - requires library to validate?
+//   // "bsonobjectid", // bson object ID
+
+// // "uri",          // an URI as parsed by Golang net/url.ParseRequestURI
+// // "email",        // an email address as parsed by Golang net/mail.ParseAddress
+// // use wildcardHostname
+// "hostname",     // a valid representation for an Internet host name, as defined by RFC 1034, section 3.1 [RFC1034].
+
+// // use utils/string/isIpv4
+// "ipv4",         // an IPv4 IP as parsed by Golang net.ParseIP
+
+// // "ipv6",         // an IPv6 IP as parsed by Golang net.ParseIP
+
+// //use isValidCIDR
+// "cidr",         // a CIDR as parsed by Golang net.ParseCIDR
+
+// //use isValidMac
+// "mac",          // a MAC address as parsed by Golang net.ParseMAC
+
+// // use toLowerCase() instead of mode modifier
+// "uuid",         // an UUID that allows uppercase defined by the regex (?i)^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}$
+// "uuid3",        // an UUID3 that allows uppercase defined by the regex (?i)^[0-9a-f]{8}-?[0-9a-f]{4}-?3[0-9a-f]{3}-?[0-9a-f]{4}-?[0-9a-f]{12}$
+// "uuid4",        // an UUID4 that allows uppercase defined by the regex (?i)^[0-9a-f]{8}-?[0-9a-f]{4}-?4[0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$
+// "uuid5",        // an UUID6 that allows uppercase defined by the regex (?i)^[0-9a-f]{8}-?[0-9a-f]{4}-?5[0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$
+
+// //use https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s13.html
+// "isbn",         // an ISBN10 or ISBN13 number string like "0321751043" or "978-0321751041"
+// "isbn10",       // an ISBN10 number string like "0321751043"
+// "isbn13",       // an ISBN13 number string like "978-0321751041"
+
+// "creditcard",   // a credit card number defined by the regex ^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$ with any non digit characters mixed in
+// "ssn",          // a U.S. social security number following the regex ^\\d{3}[- ]?\\d{2}[- ]?\\d{4}$
+// "hexcolor",     // an hexadecimal color code like "#FFFFFF", following the regex ^#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$
+
+// // /^rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)$/
+// "rgbcolor",     // an RGB color code like rgb like "rgb(255,255,2559"
+
+// //use base64Decode?
+// "byte",         // base64 encoded binary data
+
+// "password",     // any kind of string
+
+// "date",         // a date string like "2006-01-02" as defined by full-date in RFC3339
+// "duration",     // a duration string like "22 ns" as parsed by Golang time.ParseDuration or compatible with Scala duration format
+// "datetime",     // a date time string like "2014-12-15T19:30:20.000Z" as defined by date-time in RFC3339}
+// }
+
 /**
  *
  * @param t this.$store.getters[i18n/t]
