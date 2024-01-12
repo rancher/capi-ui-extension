332610454c4226ef5880e563a52009bb04a9493c
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2024-01-12 15:07:31 -0800

    add clusterclass action to create cluster from class

---
+++ pkg/capi/models/cluster.x-k8s.io.clusterclass.js
@@ -0,0 +1,33 @@
+import SteveModel from '@shell/plugins/steve/steve-class';
+import { CAPI } from '@shell/config/types';
+import { BLANK_CLUSTER, QUERY_PARAMS } from '../types/capi';
+
+export default class ClusterClass extends SteveModel {
+  get _availableActions() {
+    const out = super._availableActions;
+
+    out.unshift({
+      action:   'goToCreateCluster',
+      label:   this.t('action.createCluster'),
+      icon:     'icon icon-plus',
+      enabled:  true
+    });
+
+    return out;
+  }
+
+  goToCreateCluster() {
+    const escapedID = escape(this.id);
+    const location = {
+      name:   'c-cluster-product-resource-create',
+      params: {
+        cluster:  BLANK_CLUSTER,
+        product:  'manager',
+        resource: CAPI.CAPI_CLUSTER
+      },
+      query: { [QUERY_PARAMS.CLASS]: escapedID }
+    };
+
+    this.currentRouter().push(location);
+  }
+}
